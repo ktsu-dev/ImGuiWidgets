d301b3df0980ddf6e45e20482faa417848f9b65b
matt-edmondson <matthew.edmondson@gmail.com>
2024-01-16 17:39:01 +1100

    Add DividerContainers and zones from ImGuiApp

---
+++ ImGuiWidgets/DividerContainer.cs
@@ -0,0 +1,351 @@
+namespace ktsu.io.ImGuiWidgets;
+
+using System.Numerics;
+using ImGuiNET;
+using Extensions;
+using System.Drawing;
+using System.Collections.ObjectModel;
+
+/// <summary>
+/// An enum to specify the layout direction of the divider container.
+/// </summary>
+public enum DividerLayout
+{
+	/// <summary>
+	/// The container will be laid out in columns.
+	/// </summary>
+	Columns,
+	/// <summary>
+	/// The container will be laid out in rows.
+	/// </summary>
+	Rows,
+}
+
+/// <summary>
+/// A container that can be divided into dragable zones.
+/// Useful for creating resizable layouts.
+/// Containers can be nested to create complex layouts.
+/// </summary>
+public class DividerContainer
+{
+	/// <summary>
+	/// An ID for the container.
+	/// </summary>
+	public string Id { get; init; }
+	private DividerLayout Layout { get; }
+	private int DragIndex { get; set; } = -1;
+	private List<DividerZone> Zones { get; } = new();
+	private Action<DividerContainer>? OnResized { get; set; }
+
+	/// <summary>
+	/// Create a new divider container with a callback for when the container is resized and a specified layout direction.
+	/// </summary>
+	/// <param name="id">The ID of the container.</param>
+	/// <param name="onResized">A callback for when the container is resized.</param>
+	/// <param name="layout">The layout direction of the container.</param>
+	public DividerContainer(string id, Action<DividerContainer>? onResized, DividerLayout layout)
+	{
+		Id = id;
+		Layout = layout;
+		OnResized = onResized;
+	}
+
+	/// <summary>
+	/// Create a new divider container with the default layout direction of columns.
+	/// </summary>
+	/// <param name="id">The ID of the container.</param>
+	public DividerContainer(string id)
+		: this(id, null, DividerLayout.Columns)
+	{
+	}
+
+	/// <summary>
+	/// Create a new divider container with a specified layout direction.
+	/// </summary>
+	/// <param name="id">The ID of the container.</param>
+	/// <param name="layout">The layout direction of the container.</param>
+	public DividerContainer(string id, DividerLayout layout)
+		: this(id, null, layout)
+	{
+	}
+
+	/// <summary>
+	/// Create a new divider container with a callback for when the container is resized and the default layout direction of columns.
+	/// </summary>
+	/// <param name="id">The ID of the container.</param>
+	/// <param name="onResized">A callback for when the container is resized.</param>
+	public DividerContainer(string id, Action<DividerContainer>? onResized)
+		: this(id, onResized, DividerLayout.Columns)
+	{
+	}
+
+	/// <summary>
+	/// Tick the container to update and draw its contents.
+	/// </summary>
+	/// <param name="dt">The delta time since the last tick.</param>
+	/// <exception cref="NotImplementedException">Thrown if the layout direction is not supported.</exception>
+	public void Tick(float dt)
+	{
+		var style = ImGui.GetStyle();
+		var windowPadding = style.WindowPadding;
+		var drawList = ImGui.GetWindowDrawList();
+		var containerSize = ImGui.GetWindowSize() - (windowPadding * 2.0f);
+
+		var layoutMask = Layout switch
+		{
+			DividerLayout.Columns => new Vector2(1, 0),
+			DividerLayout.Rows => new Vector2(0, 1),
+			_ => throw new NotImplementedException(),
+		};
+
+		var layoutMaskInverse = Layout switch
+		{
+			DividerLayout.Columns => new Vector2(0, 1),
+			DividerLayout.Rows => new Vector2(1, 0),
+			_ => throw new NotImplementedException(),
+		};
+
+		var windowPos = ImGui.GetWindowPos();
+		var advance = windowPos + windowPadding;
+
+		Vector2 CalculateAdvance(DividerZone z)
+		{
+			var advance = containerSize * z.Size * layoutMask;
+
+			var first = Zones.First();
+			var last = Zones.Last();
+			if (first != last && z == first)
+			{
+				advance += windowPadding * 0.5f * layoutMask;
+			}
+
+			return new Vector2((float)Math.Round(advance.X), (float)Math.Round(advance.Y));
+		}
+
+		Vector2 CalculateZoneSize(DividerZone z)
+		{
+			var zoneSize = (containerSize * z.Size * layoutMask) + (containerSize * layoutMaskInverse);
+
+			var first = Zones.First();
+			var last = Zones.Last();
+			if (first != last)
+			{
+				if (z == first || z == last)
+				{
+					zoneSize -= windowPadding * 0.5f * layoutMask;
+				}
+				else
+				{
+					zoneSize -= windowPadding * layoutMask;
+				}
+			}
+
+			return new Vector2((float)Math.Floor(zoneSize.X), (float)Math.Floor(zoneSize.Y));
+		}
+
+		ImGui.SetNextWindowPos(advance);
+		ImGui.BeginChild(Id, containerSize, ImGuiChildFlags.None, ImGuiWindowFlags.NoSavedSettings);
+
+		foreach (var z in Zones)
+		{
+			var zoneSize = CalculateZoneSize(z);
+			ImGui.SetNextWindowPos(advance);
+			ImGui.BeginChild(z.Id, zoneSize, ImGuiChildFlags.Border, ImGuiWindowFlags.NoSavedSettings);
+			z.Tick(dt);
+			ImGui.EndChild();
+
+			advance += CalculateAdvance(z);
+		}
+
+		ImGui.EndChild();
+
+		//render the handles last otherwise they'll be covered by the other zones windows and wont receive hover events
+
+		//reset the advance to the top left of the container
+		advance = windowPos + windowPadding;
+		float resize = 0;
+		var mousePos = ImGui.GetMousePos();
+		foreach (var (z, i) in Zones.WithIndex())
+		{
+			//draw the grab handle if we're not the last zone
+			if (z != Zones.Last())
+			{
+				var zoneSize = CalculateZoneSize(z);
+				var lineA = advance + (zoneSize * layoutMask) + (windowPadding * 0.5f * layoutMask);
+				var lineB = lineA + (zoneSize * layoutMaskInverse);
+				float lineWidth = style.WindowPadding.X * 0.5f;
+				float grabWidth = style.WindowPadding.X * 2;
+				var grabBox = new Vector2(grabWidth, grabWidth) * 0.5f;
+				var grabMin = lineA - (grabBox * layoutMask);
+				var grabMax = lineB + (grabBox * layoutMask);
+				var grabSize = grabMax - grabMin;
+				RectangleF handleRect = new(grabMin.X, grabMin.Y, grabSize.X, grabSize.Y);
+				bool handleHovered = handleRect.Contains(mousePos.X, mousePos.Y);
+				bool mouseClickedThisFrame = ImGui.IsMouseClicked(ImGuiMouseButton.Left);
+				if (handleHovered && mouseClickedThisFrame)
+				{
+					DragIndex = i;
+				}
+
+				if (DragIndex == i)
+				{
+					if (ImGui.IsMouseDown(ImGuiMouseButton.Left))
+					{
+						var mousePosLocal = mousePos - advance;
+
+						var first = Zones.First();
+						var last = Zones.Last();
+						if (first != last && z != first)
+						{
+							mousePosLocal += windowPadding * 0.5f * layoutMask;
+						}
+
+						float requestedSize = Layout switch
+						{
+							DividerLayout.Columns => mousePosLocal.X / containerSize.X,
+							DividerLayout.Rows => mousePosLocal.Y / containerSize.Y,
+							_ => throw new NotImplementedException(),
+						};
+						resize = Math.Clamp(requestedSize, 0.1f, 0.9f);
+					}
+					else
+					{
+						DragIndex = -1;
+					}
+				}
+
+				var lineColor = DragIndex == i ? new Vector4(1, 1, 1, 0.7f) : handleHovered ? new Vector4(1, 1, 1, 0.5f) : new Vector4(1, 1, 1, 0.3f);
+				//drawList.AddRectFilled(grabMin, grabMax, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 0.3f)));
+				drawList.AddLine(lineA, lineB, ImGui.ColorConvertFloat4ToU32(lineColor), lineWidth);
+
+				if (handleHovered || DragIndex == i)
+				{
+					ImGui.SetMouseCursor(Layout switch
+					{
+						DividerLayout.Columns => ImGuiMouseCursor.ResizeEW,
+						DividerLayout.Rows => ImGuiMouseCursor.ResizeNS,
+						_ => throw new NotImplementedException(),
+					});
+				}
+			}
+
+			advance += CalculateAdvance(z);
+		}
+
+		//do the actual resize at the end of the tick so that we don't mess with the dimensions of the layout mid rendering
+		if (DragIndex > -1)
+		{
+			var resizedZone = Zones[DragIndex];
+			var neighbourZone = Zones[DragIndex + 1];
+			float combinedSize = resizedZone.Size + neighbourZone.Size;
+			float maxSize = combinedSize - 0.1f;
+			resize = Math.Clamp(resize, 0.1f, maxSize);
+			bool sizeDidChange = resizedZone.Size != resize;
+			resizedZone.Size = resize;
+			neighbourZone.Size = combinedSize - resize;
+			if (sizeDidChange)
+			{
+				OnResized?.Invoke(this);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Add a zone to the container.
+	/// </summary>
+	/// <param name="id">The ID of the zone.</param>
+	/// <param name="size">The size of the zone.</param>
+	/// <param name="resizable">Whether the zone is resizable.</param>
+	/// <param name="tickDelegate">The delegate to call when the zone is ticked.</param>
+	public void Add(string id, float size, bool resizable, Action<float> tickDelegate) => Zones.Add(new(id, size, resizable, tickDelegate));
+
+	/// <summary>
+	/// Add a zone to the container.
+	/// </summary>
+	/// <param name="id">The ID of the zone.</param>
+	/// <param name="size">The size of the zone.</param>
+	/// <param name="tickDelegate">The delegate to call when the zone is ticked.</param>
+	public void Add(string id, float size, Action<float> tickDelegate) => Zones.Add(new(id, size, tickDelegate));
+
+	/// <summary>
+	/// Add a zone to the container.
+	/// </summary>
+	/// <param name="id">The ID of the zone.</param>
+	/// <param name="tickDelegate">The delegate to call when the zone is ticked.</param>
+	public void Add(string id, Action<float> tickDelegate)
+	{
+		float size = 1.0f / (Zones.Count + 1);
+		Zones.Add(new(id, size, tickDelegate));
+	}
+
+	/// <summary>
+	/// Remove a zone from the container.
+	/// </summary>
+	/// <param name="id">The ID of the zone to remove.</param>
+	public void Remove(string id)
+	{
+		var zone = Zones.FirstOrDefault(z => z.Id == id);
+		if (zone != null)
+		{
+			Zones.Remove(zone);
+		}
+	}
+
+	/// <summary>
+	/// Remome all zones from the container.
+	/// </summary>
+	public void Clear() => Zones.Clear();
+
+	/// <summary>
+	/// Set the size of a zone by its ID.
+	/// </summary>
+	/// <param name="id">The ID of the zone to set the size of.</param>
+	/// <param name="size">The size to set.</param>
+	public void SetSize(string id, float size)
+	{
+		var zone = Zones.FirstOrDefault(z => z.Id == id);
+		if (zone != null)
+		{
+			zone.Size = size;
+		}
+	}
+
+	/// <summary>
+	/// Set the size of a zone by its index.
+	/// </summary>
+	/// <param name="index">The index of the zone to set the size of.</param>
+	/// <param name="size">The size to set.</param>
+	public void SetSize(int index, float size)
+	{
+		if (index >= 0 && index < Zones.Count)
+		{
+			Zones[index].Size = size;
+		}
+	}
+
+	/// <summary>
+	/// Set the sizes of all zones in this container from a collection of sizes.
+	/// </summary>
+	/// <param name="sizes">The collection of sizes to set.</param>
+	/// <exception cref="ArgumentException"></exception>
+	public void SetSizesFromList(ReadOnlyCollection<float> sizes)
+	{
+		ArgumentNullException.ThrowIfNull(sizes, nameof(sizes));
+
+		if (sizes.Count != Zones.Count)
+		{
+			throw new ArgumentException("List of sizes must be the same length as the zones list");
+		}
+
+		for (int i = 0; i < sizes.Count; i++)
+		{
+			Zones[i].Size = sizes[i];
+		}
+	}
+
+	/// <summary>
+	/// Get a list of the sizes of all zones in this container.
+	/// </summary>
+	/// <returns>A list of the sizes of all zones in this container.</returns>
+	public Collection<float> GetSizes() => (Collection<float>)Zones.Select(z => z.Size);
+}
---
+++ ImGuiWidgets/DividerZone.cs
@@ -0,0 +1,77 @@
+namespace ktsu.io.ImGuiWidgets;
+
+/// <summary>
+/// A zone that can be resized by dragging a divider.
+/// For use inside DividerContainer.
+/// </summary>
+public class DividerZone
+{
+	/// <summary>
+	/// The unique identifier for this zone.
+	/// </summary>
+	public string Id { get; private set; }
+	/// <summary>
+	/// The size of this zone.
+	/// </summary>
+	public float Size { get; set; }
+	/// <summary>
+	/// Whether this zone can be resized.
+	/// </summary>
+	public bool Resizable { get; } = true;
+	private Action<float>? TickDelegate { get; }
+
+	/// <summary>
+	/// Create a new divider zone.
+	/// </summary>
+	/// <param name="id">The unique identifier for this zone.</param>
+	/// <param name="size">The size of this zone.</param>
+	public DividerZone(string id, float size)
+	{
+		Id = id;
+		Size = size;
+		Resizable = true;
+	}
+
+	/// <summary>
+	/// Create a new resizable divider zone with a tick delegate.
+	/// </summary>
+	/// <param name="id">The unique identifier for this zone.</param>
+	/// <param name="size">The size of this zone.</param>
+	/// <param name="tickDelegate">The delegate to call every frame.</param>
+	public DividerZone(string id, float size, Action<float> tickDelegate)
+	{
+		Id = id;
+		Size = size;
+		TickDelegate = tickDelegate;
+	}
+
+	/// <summary>
+	/// Create a new divider zone that is optionally resizable  with a tick delegate.
+	/// </summary>
+	/// <param name="id">The unique identifier for this zone.</param>
+	/// <param name="size">The size of this zone.</param>
+	/// <param name="resizable">Whether this zone can be resized.</param>
+	/// <param name="tickDelegate">The delegate to call every frame.</param>
+	public DividerZone(string id, float size, bool resizable, Action<float> tickDelegate)
+	{
+		Id = id;
+		Size = size;
+		Resizable = resizable;
+		TickDelegate = tickDelegate;
+	}
+
+	/// <summary>
+	/// Create a new divider zone that is optionally resizable.
+	/// </summary>
+	/// <param name="id">The unique identifier for this zone.</param>
+	/// <param name="size">The size of this zone.</param>
+	/// <param name="resizable">Whether this zone can be resized.</param>
+	public DividerZone(string id, float size, bool resizable)
+	{
+		Id = id;
+		Size = size;
+		Resizable = resizable;
+	}
+
+	internal void Tick(float dt) => TickDelegate?.Invoke(dt);
+}
