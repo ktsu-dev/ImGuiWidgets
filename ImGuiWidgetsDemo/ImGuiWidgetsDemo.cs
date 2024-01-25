f05e95b2da7c6b4a3927989b4466fed50b4a1c48
matt-edmondson <matthew.edmondson@gmail.com>
2024-01-25 11:44:41 +1100

    Fix mismatched ImGui version, add a demo project, use multilevel directory props instead of project.props files

---
+++ ImGuiWidgetsDemo/ImGuiWidgetsDemo.cs
@@ -0,0 +1,33 @@
+namespace ktsu.io.ImGuiWidgetsDemo;
+
+using ImGuiNET;
+using ktsu.io.ImGuiApp;
+using ktsu.io.ImGuiWidgets;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
+internal class ImGuiWidgetsDemo
+{
+	private static void Main(string[] args)
+	{
+		ImGuiWidgetsDemo imGuiWidgetsDemo = new();
+		ImGuiApp.Start(nameof(ImGuiWidgetsDemo), new ImGuiAppWindowState(), imGuiWidgetsDemo.Tick, imGuiWidgetsDemo.ShowMenu, imGuiWidgetsDemo.WindowResized);
+	}
+
+	private static float value = 0.5f;
+	private void Tick(float dt)
+	{
+		float ms = dt * 1000;
+		Knob.Draw("DT", ref ms, 0, 10, 150f);
+		ImGui.SameLine();
+		Knob.Draw("Value", ref value, 0f, 1f, 150f);
+	}
+
+	private void ShowMenu()
+	{
+	}
+
+	private void WindowResized()
+	{
+	}
+}
