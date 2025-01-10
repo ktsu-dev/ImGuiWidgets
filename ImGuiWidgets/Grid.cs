f018882a5db1e972a3b6ff35453ac23e767006d9
Damon Lewis <damon@thethreethousands.com>
2025-01-10 15:58:04 +1100

    Updated comments for ColumnMajor enum

--- ImGuiWidgets/Grid.cs
+++ ImGuiWidgets/Grid.cs
@@ -38,17 +38,15 @@ public static partial class ImGuiWidgets
 		/// <summary>
 		/// Items are displayed top to bottom before moving to the next column.
 		/// Recommended when displaying tables of data.
-		/// Note: This will never display uneven rows that have more than 1 item
-		/// missing relative to them.
 		/// Example:
-		/// [ [1] [3] [5] ]
-		/// [ [2] [4] [6] ]
+		/// [ [1] [4] ]
+		/// [ [2] [5] ]
+		/// [ [3] [6] ]
 		/// OR
-		/// [ [1] [3] [5] ]
-		/// [ [2] [4]     ]
-		/// NEVER
-		/// [ [1] [3] [5] ]
-		/// [ [2]         ]
+		/// [ [1] [5] ]
+		/// [ [2] [6] ]
+		/// [ [3]     ]
+		/// [ [4]     ]
 		/// </summary>
 		ColumnMajor,
 	}
